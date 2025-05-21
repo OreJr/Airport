ad67abdd81e94b38ee998d42c5c80d7ea0a9f41e
unknown <orested@uninorte.edu.co>
2025-05-21 02:14:53 -0500

    creación de clase storage y  base para el  controlador del  pasajero

--- src/core/controllers/PassengerControler.java
+++ src/core/controllers/PassengerControler.java
@@ -4,10 +4,42 @@
  */
 package core.controllers;
 
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+import core.models.Passenger;
+import core.models.storage.Storage;
+import java.time.LocalDate;
+
 /**
  *
  * @author OreJr
  */
 public class PassengerControler {
-    
+
+    public static Response addPassenger(long id, String firstname, String lastname, int year, int month, int day, int phoneCode, long phone, String country) {
+        try {
+            //antes de  establecer el LocalDate, validar  año,  mes y  dia
+            LocalDate birthDate = LocalDate.of(year, month, day);
+            if (firstname.equals("")) {
+                return new Response("Firstname must be not empty", Status.BAD_REQUEST);
+            }
+
+            if (lastname.equals("")) {
+                return new Response("Lastname must be not empty", Status.BAD_REQUEST);
+            }
+
+            if (country.equals("")) {
+                return new Response("country must be not empty", Status.BAD_REQUEST);
+            }
+
+            Storage storage = Storage.getInstance();
+            if (!storage.addPassenger(new Passenger(id, firstname, lastname, birthDate, phoneCode, phone, country))) {
+                return new Response("A passenger with that id already exists", Status.BAD_REQUEST);
+            }
+
+            return new Response("Person created successfully", Status.CREATED);
+        } catch (Exception ex) {
+            return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ src/core/models/storage/Storage.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.models.storage;
+
+import core.models.Passenger;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author OreJr
+ */
+public class Storage {
+    // Instancia Singleton
+    private static Storage instance;
+    
+    // Atributos del Storage
+    private ArrayList<Passenger> passengers;
+    
+    private Storage() {
+        this.passengers = new ArrayList<>();
+    }
+    
+    public static Storage getInstance() {
+        if (instance == null) {
+            instance = new Storage();
+        }
+        return instance;
+    }
+    
+    public boolean addPassenger(Passenger passenger) {
+        for (Passenger p : this.passengers) {
+            if (p.getId() == passenger.getId()) {
+                return false;
+            }
+        }
+        this.passengers.add(passenger);
+        return true;
+    }
+    
+    public Passenger getPassenger(long id) {
+        for (Passenger passenger : this.passengers) {
+            if (passenger.getId() == id) {
+                return passenger;
+            }
+        }
+        return null;
+    }
+}
